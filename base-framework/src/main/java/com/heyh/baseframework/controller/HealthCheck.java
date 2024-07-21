fc55bec6854ad0da06568a9afb8171d7d87bf21a
heyh <hosystem@163.com>
2024-07-21 14:06:10 +0800

    [feat] swagger 测试使用

--- base-framework/src/main/java/com/heyh/baseframework/controller/HealthCheck.java
+++ base-framework/src/main/java/com/heyh/baseframework/controller/HealthCheck.java
@@ -1,14 +1,18 @@
 package com.heyh.baseframework.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //@RestController = @Controller + @@ResponseBody
 @RequestMapping("/health")
+@Api(tags = "项目健康检查")
 public class HealthCheck {
 
+    @ApiOperation("say hello")
     @GetMapping("/hello")//    @GetMapping("/hello") = @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String sayHello(){
         return "hello";
